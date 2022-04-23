25b56df40fb6873c1d72a828af0b8f2c858615fa
anna.osinska <annaosinska1988@gmail.com>
2022-04-23 19:37:15 +0200

    Add new test - test_add_contact

---
+++ contact.py
@@ -0,0 +1,32 @@
+
+class Contact:
+
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company,
+                 address, telephone_home, telephone_mobile, telephone_work, email, email2,email3,
+                 homepage, bday, bmonth, byear, aday, amonth, ayear, address2):
+
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.telephone_home = telephone_home
+        self.telephone_mobile = telephone_mobile
+        self.telephone_work = telephone_work
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday =bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+
+
+
+
