a91496eed6807d1b3df85a691971aa462094b18c
anna.osinska <annaosinska1988@gmail.com>
2022-06-27 19:20:13 +0200

    Task 21

--- test/test_contact_data.py
+++ test/test_contact_data.py
@@ -1,14 +1,13 @@
 import re
 
+from model.contact import Contact
 
-def test_contact_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.first_name == contact_from_edit_page.first_name
-    assert contact_from_home_page.last_name == contact_from_edit_page.last_name
+
+def test_contact_on_home_page(app, db):
+    contact_from_home_page = app.contact.get_contact_list()
+    contact_from_db = db.get_contact_list()
+
+    assert sorted(contact_from_home_page, key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
 
 
 def test_phones_on_contact_view_page(app):
